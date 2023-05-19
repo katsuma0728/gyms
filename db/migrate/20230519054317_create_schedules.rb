4ea09acad5cbec341764c590bd33fcd1decead44
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-05-19 06:19:07 +0000

    datebase

---
+++ db/migrate/20230519054317_create_schedules.rb
@@ -0,0 +1,13 @@
+class CreateSchedules < ActiveRecord::Migration[6.1]
+  def change
+    create_table :schedules do |t|
+      
+      t.integer :user_id,     foreign_key: true
+      t.string :title,        null: false, default: ""
+      t.text :memo,           null: false, default: ""
+      t.datetime :start_time, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230519060657_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      
+      t.integer :user_id, foreign_key: true
+      t.string :title,    null: false, default: ""
+      t.text :blog,       null: false, default: ""
+      t.integer :status,  null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
