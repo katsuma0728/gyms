791f2bb61d4fc8bd26d5756a5b407f80090eb4ae
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-06-25 12:01:42 +0000

    Ajax

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,24 +1,30 @@
 class PostCommentsController < ApplicationController
 
   def create
-    post_blog = PostBlog.find(params[:post_blog_id])
-    post_comment = current_user.post_comments.new(post_comment_params)
-    post_comment.post_blog_id = post_blog.id
-    if post_comment.save
-       redirect_to post_blog_path(post_blog.id)
+    @post_blog = PostBlog.find(params[:post_blog_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_blog_id = @post_blog.id
+    if @post_comment.save
+       #redirect_to post_blog_path(post_blog.id)
+       @post_blog = PostBlog.find(params[:post_blog_id])
+       @post_comment = current_user.post_comments.new(post_comment_params)
     else
       #エラーメッセージ表示
-      @error_comment = post_comment
+      @error_comment = @post_comment
       @post_blog = PostBlog.find(params[:post_blog_id])
       @post_comment = PostComment.new
       @post_tags = @post_blog.tags
-      render 'post_blogs/show'
+      render 'error'
     end
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to post_blog_path(params[:post_blog_id])
+
+    post_comment = PostComment.find(params[:id])
+    post_comment.destroy
+    #redirect_to post_blog_path(params[:post_blog_id])
+    @post_blog = PostBlog.find(params[:post_blog_id])
+    #@post_comment = PostComment.new
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
+import "../script.js"
 //import 'simple_calendar'
 
 // 以降のプログラムが動かなくなる
