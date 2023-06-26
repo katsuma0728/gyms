d3b51bef8ef6e7a3cbf61b63c32f64210aa9c091
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-06-26 20:51:58 +0000

    layouts

--- app/models/post_blog.rb
+++ app/models/post_blog.rb
@@ -22,12 +22,12 @@ class PostBlog < ApplicationRecord
     likes.exists?(user_id: user.id)
   end
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_fill: [width, height]).processed
   end
 
   def save_tag(sent_tags)
