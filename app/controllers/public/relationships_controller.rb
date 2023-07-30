8060e1b8384ef00c7335ec8c35bea7b639bfaf90
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-07-30 05:47:04 +0000

    [add]follow

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,12 +14,12 @@ class Public::RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+    @users = user.followings.order(created_at: :desc).page(params[:page]).per(2)
   end
   
   # フォロワー一覧
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers
+    @users = user.followers.order(created_at: :desc).page(params[:page]).per(2)
   end
 end
